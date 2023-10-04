915bbc411e8e3d56a4f0d9dc10e08ca225af922c
KKasdasd <restokin97@gmail.com>
2023-10-04 11:04:03 +0200

    Created question bank

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from data import question_data
 from question_model import Question
 
-
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
 
 
